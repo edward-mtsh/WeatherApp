d8782ec148727da8db51769ff39eb818cd499946
Edward Mtshweni <edward.mtshweni@Edwards-MacBook-Pro.local>
2017-09-15 12:41:47 +0200

    Initial Commit

---
+++ WeatherApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Edward Mtshweni on 2017/09/15.
+//  Copyright © 2017 Edward Mtshweni. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
